bd15f71c91472fdf546976a02a386a67dc122cb1
Jacob <2428207444@qq.com>
2023-08-05 15:16:54 +0800

    更新nonImg model的训练函数

--- nonImg_model_wrappers.py
+++ nonImg_model_wrappers.py
@@ -1,4 +1,4 @@
-#import pdb
+import pdb
 import sklearn
 import os
 import csv
@@ -10,7 +10,9 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.impute import KNNImputer
 import numpy as np
+from numpy import ndarray
 import pandas as pd
+from time import time as get_timestamp
 from sklearn.svm import SVC, SVR
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
@@ -44,7 +46,6 @@ note: in the tasks argument, need to put COG before ADD since imputer will be ca
 
 class NonImg_Model_Wrapper:
     def __init__(self, tasks, main_config, task_config, seed):
-
         # --------------------------------------------------------------------------------------------------------------
         # some constants
         self.seed = seed  # random seed number
@@ -54,6 +55,10 @@ class NonImg_Model_Wrapper:
         self.n_tasks = len(tasks)  # number of tasks will be trained
         self.tasks = tasks  # a list of tasks names to be trained
         self.features = task_config['features'] # a list of features
+        if 'ADD_score' in self.features:
+            self.features.remove('ADD_score')
+        if 'COG_score' in self.features:
+            self.features.remove('COG_score')
 
         # --------------------------------------------------------------------------------------------------------------
         # folders preparation to save checkpoints of model weights *.pth
@@ -74,13 +79,49 @@ class NonImg_Model_Wrapper:
         # initialize data
         self.train_data = []                      # note: self.train_data[i] contains the
         self.imputer = None
+        self.train_set = pd.read_csv(os.path.join(self.csv_dir, 'preprocessed_train.csv'))
+        self.valid_set = pd.read_csv(os.path.join(self.csv_dir, 'preprocessed_valid.csv'))
+        """
         self.load_preprocess_data()               #       features and labels for the i th task
+        self.valid_set = self.load_dataset(os.path.join(self.csv_dir, 'valid.csv'))
+        self.test_set = self.load_dataset(os.path.join(self.csv_dir, 'test.csv'))
+        self.train_data[0].to_csv(os.path.join(self.csv_dir, 'preprocessed_train.csv'), index=False)
+        self.valid_set.to_csv(os.path.join(self.csv_dir, 'preprocessed_valid.csv'), index=False)
+        self.test_set.to_csv(os.path.join(self.csv_dir, 'preprocessed_test.csv'), index=False)
+        """
+
+        print('初始化完毕')
 
     def train(self):
-        for i, task in enumerate(self.tasks):
-            X, Y = self.train_data[i].drop([task], axis=1), self.train_data[i][task]
-            self.models[i].fit(X, Y)
-            print(task + ' model training is done!')
+        x_train, y_train = self.train_set.drop(['COG'], axis=1).to_numpy(), self.train_set['COG'].values
+        x_valid, y_valid = self.valid_set.drop(['COG'], axis=1).to_numpy(), self.valid_set['COG'].values
+        model = CatBoostRegressor(iterations=1, learning_rate=0.05)
+        for epoch in range(100):
+            # 训练、保存模型
+            if epoch != 0:
+                init_model = model
+            else:
+                init_model = None
+            model.fit(x_train, y_train, init_model=init_model, verbose=False)
+            model.save_model(os.path.join(self.checkpoint_dir, 'CatBoostRegressor_{}'.format(epoch)))
+
+            # 验证模型
+            pred_train = self.get_three_classes_prediction(model.predict(x_train))
+            train_accuracy = sum(pred_train == y_train) / y_train.shape[0]
+            pred_valid = self.get_three_classes_prediction(model.predict(x_valid))
+            valid_accuracy = sum(pred_valid == y_valid) / y_valid.shape[0]
+            print('Epoch {}: train_accuracy={:.4f} -- valid_accuracy={:.4f}'.format(
+                epoch + 1, train_accuracy, valid_accuracy
+            ))
+
+    def get_three_classes_prediction(self, scores: ndarray, thresholds: tuple = (0.5, 1.5)) -> ndarray:
+        prediction = np.zeros(len(scores), 'int')
+        for i in range(len(scores)):
+            if scores[i] > thresholds[1]:
+                prediction[i] = 2
+            elif scores[i] >= thresholds[0]:
+                prediction[i] = 1
+        return prediction
 
     def get_optimal_thres(self, csv_name='valid'):
         self.gen_score(stages=[csv_name])
@@ -273,17 +314,18 @@ class NonImg_Model_Wrapper:
         and fit the imputer based on the COG cases from the training part
         """
         imputation_method = self.config['impute_method']
-        if imputation_method == 'mean':
+        #print(imputation_method)
+        if imputation_method == 'mean':  # 均值填充
             imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-        elif imputation_method == 'median':
+        elif imputation_method == 'median':  # 中位数填充
             imp = SimpleImputer(missing_values=np.nan, strategy='median')
-        elif imputation_method == 'most_frequent':
+        elif imputation_method == 'most_frequent':  # 众数填充
             imp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
-        elif imputation_method == 'constant':
+        elif imputation_method == 'constant':  # 常数0填充
             imp = SimpleImputer(missing_values=np.nan, strategy='constant')
-        elif imputation_method == 'KNN':
+        elif imputation_method == 'KNN':  # K邻近法填充
             imp = KNNImputer(n_neighbors=20)
-        elif imputation_method == 'Multivariate':
+        elif imputation_method == 'Multivariate':  # 多元填补
             imp = IterativeImputer(max_iter=1000)
         else:
             raise NameError('method for imputation not supported')
@@ -291,19 +333,26 @@ class NonImg_Model_Wrapper:
         return imp
 
     def load_preprocess_data(self):
-        data_train = pd.read_csv(self.csv_dir + 'train.csv')
+        data_train = pd.read_csv(os.path.realpath(os.path.join(self.csv_dir, 'train.csv')))
         #print("data_train.columns= ", list(data_train.columns))
         #print("data_train.len= ", len(data_train))
+        pdb.set_trace()
         for task in self.tasks:
             #print("task= ", task)
             #print("self.features + [task]= ", self.features)
-            self.train_data.append(data_train[self.features + [task]])
+            columns = list(set(self.features + [task]) & set(data_train.columns))
+            self.train_data.append(data_train[columns])
             #print("==========")
         for i, task in enumerate(self.tasks):
             self.train_data[i] = self.preprocess_pipeline(self.train_data[i], task)
-            print('after preprocess pipeline, the data frame for the {} task is'.format(task))
-            print(self.train_data[i])
-            print('\n' * 2)
+            # print('after preprocess pipeline, the data frame for the {} task is'.format(task))
+            # print(self.train_data[i])
+            # print('\n' * 2)
+
+    def load_dataset(self, dataset_path):
+        dataset = pd.read_csv(dataset_path)
+        columns = list(set(self.features + ['COG']) & set(dataset.columns))
+        return self.preprocess_pipeline(dataset[columns], 'COG')
 
     def preprocess_pipeline(self, data, task):
         """
@@ -316,12 +365,13 @@ class NonImg_Model_Wrapper:
         #print("process_pipeline   features= ", features)
         #print("len(features)= ", len(features))    #len= 66
         features = self.imputation(features) # do imputation merely on features
+        # features.to_csv(os.path.join(root_path, 'demo_train.csv'), index=False)
         features = self.normalize(features)  # normalize features
         features[task] = data[task]          # adding the task column back
         return features                      # return the complete data
 
     def drop_cases_without_label(self, data, label):
-        data = data.dropna(axis=0, how='any', thresh=None, subset=[label], inplace=False)
+        data = data.dropna(axis=0, how='any', subset=[label], inplace=False)
         return data.reset_index(drop=True)
 
     def transform_categorical_variables(self, data):
@@ -333,20 +383,19 @@ class NonImg_Model_Wrapper:
 
     def imputation(self, data):
         columns = data.columns
-        print("len(columns)= ", len(columns))
-        print("columns= ", columns)
+        #print("len(columns)= ", len(columns))
+        #print("columns= ", columns)
         if self.imputer == None:
             self.imputer = self.init_imputer(data)
-            print("==comming !!!==")
-            print("self.imputer.len= ", np.shape(self.imputer))
-        print("data.shape= ", np.shape(data))
-        print("type(data)= ", type(data))
+            #print("==comming !!!==")
+            #print("self.imputer.len= ", np.shape(self.imputer))
+        #print("data.shape= ", np.shape(data))
+        #print("type(data)= ", type(data))
         # arr = np.zeros((789, 66))
         # data11 = pd.DataFrame(arr, columns=columns)
         # print("data11.shape= ", np.shape(data11))
         data = self.imputer.transform(data)
-        print("after data.shape= ", np.shape(data))
-        #pdb.set_trace()
+        #print("after data.shape= ", np.shape(data))
         return pd.DataFrame(data, columns=columns)
 
     def normalize(self, data):
@@ -370,32 +419,55 @@ class NonImg_Model_Wrapper:
 
 class Fusion_Model_Wrapper(NonImg_Model_Wrapper):
 
-    def load_preprocess_data(self):
-        data_train = pd.read_csv(self.csv_dir + 'train.csv')
-        for task in self.tasks:
-            self.train_data.append(data_train[self.features + [task]])
-        for i, task in enumerate(self.tasks):
-            self.train_data[i] = self.preprocess_pipeline(self.train_data[i], task)
-            print('after preprocess pipeline, the data frame for the {} task is'.format(task))
-            print(self.train_data[i])
-            print('\n' * 2)
+    def __init__(self, *args, **kwargs):
+        super(Fusion_Model_Wrapper, self).__init__(*args, **kwargs)
+        self.train_set = pd.read_csv(os.path.join(self.csv_dir, 'fusion_train.csv'))
+        self.valid_set = pd.read_csv(os.path.join(self.csv_dir, 'fusion_valid.csv'))
 
+        """
+        # 加入COG_score作为输入
+        from model_eval.mri_predict_test_set import MRIModel
+        cp_dict = {}
+        checkpoint_dir_path = os.path.join(root_path, 'checkpoint_dir/MRI_only_v3')
+        for fn in os.listdir(checkpoint_dir_path):
+            cp_type, index = fn[:fn.rfind('.')].split('_')
+            index = int(index)
+            if index in cp_dict:
+                cp_dict[index][cp_type] = os.path.join(checkpoint_dir_path, fn)
+            else:
+                cp_dict[index] = {cp_type: os.path.join(checkpoint_dir_path, fn)}
+        mri_test_scores = np.load(os.path.join(root_path, 'model_eval/eval_result/mri/scores.npy'))
+        mri_eval_result = pd.read_csv(os.path.join(root_path, 'model_eval/eval_result/mri/result.csv'))
+        df = pd.DataFrame({
+            'model_index': sorted(cp_dict.keys()),
+            'accuracy': mri_eval_result['accuracy'].values
+        }).sort_values('accuracy', ascending=False)
+        best_cp = cp_dict[df.iloc[0, 0]]
+        best_model = MRIModel(best_cp['backbone'], best_cp['COG'])
 
+        def mri_generator(filenames):
+            for filename in filenames:
+                mri = np.load(os.path.join(mri_path, filename))
+                mri = np.expand_dims(np.expand_dims(mri, axis=0), axis=0)
+                yield mri
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        source_train_set = pd.read_csv(os.path.join(root_path, 'lookupcsv/CrossValid/no_cross/train_source.csv'))
+        train_cog_score = best_model.predict(
+            mri_generator(source_train_set['filename'].values),
+            source_train_set.shape[0]
+        )
+        source_valid_set = pd.read_csv(os.path.join(root_path, 'lookupcsv/CrossValid/no_cross/valid_source.csv'))
+        valid_cog_score = best_model.predict(
+            mri_generator(source_valid_set['filename'].values),
+            source_valid_set.shape[0]
+        )
+        test_cog_score = mri_test_scores[df.index[0]]
+        test_set = pd.read_csv(os.path.join(self.csv_dir, 'preprocessed_test.csv'))
+        self.train_set.insert(self.train_set.shape[1], 'COG_score', train_cog_score, allow_duplicates=False)
+        self.valid_set.insert(self.valid_set.shape[1], 'COG_score', valid_cog_score, allow_duplicates=False)
+        test_set.insert(test_set.shape[1], 'COG_score', test_cog_score, allow_duplicates=False)
+        self.train_set.to_csv(os.path.join(self.csv_dir, 'fusion_train.csv'), index=False)
+        self.valid_set.to_csv(os.path.join(self.csv_dir, 'fusion_valid.csv'), index=False)
+        test_set.to_csv(os.path.join(self.csv_dir, 'fusion_test.csv'), index=False)
+        """
 
